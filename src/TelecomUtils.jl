3753db28ef5e498ca856a1949e2ad4c132f296f3
Alberto Mengali <disberd@gmail.com>
2021-11-04 10:28:46 +0100

    add satview

--- src/TelecomUtils.jl
+++ src/TelecomUtils.jl
@@ -14,7 +14,7 @@ const k_B = 1.38064852e-23 # Boltzmann constant [m² kg / (s² K)]
 # Initialize the vector that contains the matrix to compute the beam coloring. We limit ourselves at 500 colors to start
 const F_reuse_matrix = (square = SMatrix{2,2,Float64,4}[], triangular = SMatrix{2,2,Float64,4}[])
 include("./utils.jl")
-# include(pluto_mapexpr,"./SatView.jl")
+include(pluto_mapexpr,"./SatView.jl")
 
 export generate_regular_lattice, generate_hex_lattice, generate_square_lattice, generate_rect_lattice, db2lin, lin2db, f2λ, λ2f
 export generate_colors
