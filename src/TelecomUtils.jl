5cb0f013dd9244b356f2b978e40779872f8bb629
Alberto Mengali <disberd@gmail.com>
2021-11-26 16:55:44 +0100

    Migrated to only notebooks source

--- src/TelecomUtils.jl
+++ src/TelecomUtils.jl
@@ -6,16 +6,18 @@ using StaticArrays
 using LinearAlgebra
 
 # Create the filtering functions for include calls from PlutoDevMacros
-exclude_exprs = vcat(PlutoDevMacros.default_exprlist,[Symbol("@test"), :(using InteractiveUtils)])
-pluto_mapexpr = include_mapexpr(exclude_exprs)
+# exclude_exprs = vcat(PlutoDevMacros.default_exprlist,[Symbol("@test"), :(using InteractiveUtils)])
+# pluto_mapexpr = include_mapexpr(exclude_exprs)
 # Constants
 const c₀ = 299_792_458 # Speed of light [m/s]
 const k_B = 1.38064852e-23 # Boltzmann constant [m² kg / (s² K)]
-# Initialize the vector that contains the matrix to compute the beam coloring. We limit ourselves at 500 colors to start
-const F_reuse_matrix = (square = SMatrix{2,2,Float64,4}[], triangular = SMatrix{2,2,Float64,4}[])
-include("./utils.jl")
-include(pluto_mapexpr,"./SatView.jl")
 
-export generate_regular_lattice, generate_hex_lattice, generate_square_lattice, generate_rect_lattice, db2lin, lin2db, f2λ, λ2f
-export generate_colors
+export wgs84_ellipsoid
+
+include("../notebooks/utils.jl")
+include("../notebooks/satview_basics.jl")
+include("../notebooks/satview_transformations.jl")
+include("../notebooks/satview_struct.jl")
+
+
 end
