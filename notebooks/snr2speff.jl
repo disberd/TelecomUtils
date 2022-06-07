2e9c921df8f8f8491977857f32310c1637c2784c
Alberto Mengali <disberd@gmail.com>
2022-06-07 17:26:21 +0200

    Added 5GNR capacity

--- notebooks/snr2speff.jl
+++ notebooks/snr2speff.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.17.2
+# v0.17.7
 
 # using Markdown
 # using InteractiveUtils
@@ -18,29 +18,75 @@ begin
 end
   ╠═╡ notebook_exclusive =#
 
+# ╔═╡ 1c0cf7ad-9579-4593-ae1f-441788c049ae
+#=╠═╡ notebook_exclusive
+ToC()
+  ╠═╡ notebook_exclusive =#
+
+# ╔═╡ 47a21d25-7710-4a93-8cbd-ad93cee68b74
+#=╠═╡ notebook_exclusive
+md"""
+# Packages
+"""
+  ╠═╡ notebook_exclusive =#
+
+# ╔═╡ 78ad9ad5-7c06-41e1-940d-1aad1de7d5f2
+#=╠═╡ notebook_exclusive
+md"""
+# Types
+"""
+  ╠═╡ notebook_exclusive =#
+
 # ╔═╡ 35450d8d-cda5-46ba-884e-636abf023ac1
 abstract type CapacityComputationType end
 
 # ╔═╡ a1158d4b-38cc-4c9b-a949-e94e98c2b5f6
 """
 	DVBS2x <: CapacityComputationType
-Structure used to dispatch on `speff2snr` and `snr2speff` to decide the method to use for the computation
+Structure used to dispatch on `speff2snr` and `snr2speff` to simulate the MODCODs of the DVB-S2x air interface.
+# Note
+The threshold values are taken from the error performance chapter (6) of the [DVB-S2x  standard](https://dvb.org/?standard=second-generation-framing-structure-channel-coding-and-modulation-systems-for-broadcasting-interactive-services-news-gathering-and-other-broadband-satellite-applications-part-2-dvb-s2-extensions) assuming AWGN conditions
 
-See also: [`Shannon`](@ref), [`speff2snr`](@ref), [`snr2speff`](@ref)
+See also: [`Shannon`](@ref), [`NR5G`](@ref), [`speff2snr`](@ref), [`snr2speff`](@ref)
 """
 struct DVBS2x <: CapacityComputationType end
+
+# ╔═╡ 0345b4e1-49e6-4fc2-bc0e-6f179a0b1792
+"""
+	NR5G <: CapacityComputationType
+Structure used to dispatch on `speff2snr` and `snr2speff` to simulate the MODCODs of the 5G NR air interface.
+# Note
+The thresholds for the 5G approximation are taken from a document by Intel that can be found at [this link](https://www.3gpp.org/ftp/TSG_RAN/WG1_RL1/TSGR1_90/Docs/R1-1712553.zip).
+The document present a picture of the spectral efficiency vs SNR for various 5G NR MODCODs.
+
+As the tabular data is not provided in the document, the points are extrapolated from the image using [this tool](https://automeris.io/WebPlotDigitizer/). 
+
+The thresholds extrapolated from the image with the tool are increased by 1dB, since the values from the picture refer to 10% BLER (Block Error Rate), and we want to use something more akin to QEF (Quasi Error Free) as done for the DVB-S2x thresholds.
+
+**For this reason, the results for `snr2speff` and `speff2snr` for the 5G NR waveforms will be a linear interpolation based on the extrapolated values, rather than a step function corresponding to existing MODCODs**
+
+See also: [`DVBS2x`](@ref), [`Shannon`](@ref), [`speff2snr`](@ref), [`snr2speff`](@ref)
+"""
+struct NR5G <: CapacityComputationType end
 
 # ╔═╡ 94032874-a85b-4632-bc24-c8c0154b518a
 """
 	Shannon <: CapacityComputationType
 Structure used to dispatch on `speff2snr` and `snr2speff` to decide the method to use for the computation
 
-See also: [`DVBS2x`](@ref), [`speff2snr`](@ref), [`snr2speff`](@ref)
+See also: [`DVBS2x`](@ref), [`NR5G`](@ref), [`speff2snr`](@ref), [`snr2speff`](@ref)
 """
 struct Shannon <: CapacityComputationType end
 
 # ╔═╡ 4d74427d-21e3-4736-ab29-ef65c7ea0490
 Base.broadcastable(c::CapacityComputationType) = Ref(c)
+
+# ╔═╡ a1c5f1c0-adc2-43f0-bd63-e4c6c7eca52b
+#=╠═╡ notebook_exclusive
+md"""
+# DVB-S2x Thresholds
+"""
+  ╠═╡ notebook_exclusive =#
 
 # ╔═╡ 5907b0b7-f81b-4cc3-a585-8f52fd191bae
 # Thresholds 1:5 are for VLSNR, and 4:5 have a spreading factor of 2
@@ -58,6 +104,54 @@ const n_MODCODS_DVBS2x = length(dvbs2x_thresholds_linear)
 # Find the minimum SNR needed for a MODCOD that has at least the specified spectral efficiency
 const _dvbs2x_itp_speff = LinearInterpolation((dvbs2x_modcod_speff,),1:n_MODCODS_DVBS2x; extrapolation_bc = 1)
 
+# ╔═╡ 1f389615-64d5-4084-b4a7-e7838a19ff9b
+# Find the spectral efficiency that can be achieved with DVB-S2x modcods given a certain SNR 
+const _dvbs2x_itp_snr = LinearInterpolation((dvbs2x_thresholds_linear, ), 1:n_MODCODS_DVBS2x; extrapolation_bc = n_MODCODS_DVBS2x)
+
+# ╔═╡ eac20e32-59a3-44c3-9c68-31be56ad8ee9
+#=╠═╡ notebook_exclusive
+md"""
+# 5G Thresholds
+"""
+  ╠═╡ notebook_exclusive =#
+
+# ╔═╡ ff6dd97b-66db-48e8-bd6d-bfccff28ba39
+#=╠═╡ notebook_exclusive
+md"""
+The thresholds for the 5G approximation are taken from a document by Intel that can be found at [this link](https://www.3gpp.org/ftp/TSG_RAN/WG1_RL1/TSGR1_90/Docs/R1-1712553.zip).
+The document present a picture of the spectral efficiency vs SNR for various 5G NR MODCODs.
+
+As the tabular data is not provided in the document, the points are extrapolated from the image using [this tool](https://automeris.io/WebPlotDigitizer/). 
+
+The thresholds extrapolated from the image with the tool are increased by 1dB, since the values from the picture refer to 10% BLER (Block Error Rate), and we want to use something more akin to QEF (Quasi Error Free) as done for the DVB-S2x thresholds
+"""
+  ╠═╡ notebook_exclusive =#
+
+# ╔═╡ e48e3061-4004-4cee-8a93-7218e3ba5bbc
+# We add 1 to the SNR as the thresholds were derived for a 10% BLER, we want to make it QEF
+const nr5g_thresholds_linear = 1 .+ [-7, -6.154185022026431, -5.429200755191944, -4.583385777218375, -3.737570799244807, -2.8112020138451843, -2.0862177470106973, -1.3612334801762085, -0.6362492133417224, 0.2498426683448738, 1.0151038388923883, 1.9011957205789844, 2.4650723725613606, 2.9886721208307137, 3.75393329137823, 4.559471365638769, 5.244178728760229, 5.848332284455637, 6.61359345500315, 7.419131529263693, 8.506607929515424, 9.513530522341098, 10.157960981749534, 11.003775959723102, 12.010698552548776, 12.775959723096294, 13.50094398993078, 14.427312775330405, 15.152297042164887, 15.917558212712406, 16.60226557583387, 17.488357457520458, 18.45500314663311, 19.3410950283197, 19.98552548772814, 20.509125235997487, 20.952171176840785, 21.677155443675275, 22.402139710509758, 23.00629326620517, 23.73127753303966, 24.49653870358717, 25.10069225928258] |> x -> 10 .^ (x./10)
+
+# ╔═╡ d2fbd3c2-215e-4859-861e-03a0b2efdc9b
+const nr5g_modcods_speff = [0.20496894409937916,  0.22981366459627317,  0.279503105590063,  0.341614906832298,  0.44099378881987583,  0.5031055900621126,  0.6024844720496905,  0.6956521739130421,  0.7888198757763982,  0.9254658385093171,  1.0496894409937898,  1.1739130434782608,  1.2608695652173916,  1.3354037267080754,  1.4596273291925472,  1.633540372670808,  1.7950310559006217,  1.9689440993788825,  2.167701863354038,  2.366459627329193,  2.6149068322981375,  2.863354037267081,  3.024844720496895,  3.2360248447204976,  3.4844720496894412,  3.695652173913044,  3.9440993788819885,  4.229813664596274,  4.465838509316771,  4.6645962732919255,  4.850931677018634,  5.099378881987578,  5.360248447204969,  5.658385093167702,  5.919254658385094,  6.093167701863354,  6.242236024844721,  6.490683229813666,  6.714285714285715,  6.91304347826087,  7.099378881987578,  7.2981366459627335,  7.409937888198758]
+
+# ╔═╡ b85c0bff-162d-402e-8d85-44d46b4af8a8
+const n_MODCODS_5GNR = length(nr5g_thresholds_linear)
+
+# ╔═╡ c2062a21-8027-4d49-85d6-6666f761cabb
+# Find the spectral efficiency that can be achieved with 5G NR given a certain SNR 
+const _nr5g_itp_snr = LinearInterpolation((nr5g_thresholds_linear, ), nr5g_modcods_speff; extrapolation_bc = Flat())
+
+# ╔═╡ 29d90f34-0aeb-48d3-80b4-759bb0b77c2f
+# Find the minimum SNR needed for a MODCOD that has at least the specified spectral efficiency
+const _nr5g_itp_speff = LinearInterpolation((nr5g_modcods_speff,),nr5g_thresholds_linear; extrapolation_bc = Flat())
+
+# ╔═╡ 712f157b-8cc9-4034-8f54-8b57a7953683
+#=╠═╡ notebook_exclusive
+md"""
+# Exported Functions
+"""
+  ╠═╡ notebook_exclusive =#
+
 # ╔═╡ 4b14629a-b25f-4f3c-94bd-14e77c044695
 """
 	speff2snr(DVBS2x(), η)
@@ -69,10 +163,6 @@ The first argument specifies whether to use the DVBS2x thresholds or the Shannon
 See also: [`DVBS2x`](@ref), [`Shannon`](@ref), [`snr2speff`](@ref)
 """
 speff2snr(::DVBS2x, η) = η > dvbs2x_modcod_speff[end] ? NaN : dvbs2x_thresholds_linear[floor(Int,_dvbs2x_itp_speff(η))]
-
-# ╔═╡ 1f389615-64d5-4084-b4a7-e7838a19ff9b
-# Find the spectral efficiency that can be achieved with DVB-S2x modcods given a certain SNR 
-const _dvbs2x_itp_snr = LinearInterpolation((dvbs2x_thresholds_linear, ), 1:n_MODCODS_DVBS2x; extrapolation_bc = n_MODCODS_DVBS2x)
 
 # ╔═╡ 215ca230-77fd-4bbb-a4f7-8af1a0c9bcb3
 """
@@ -86,6 +176,12 @@ See also: [`DVBS2x`](@ref), [`Shannon`](@ref), [`speff2snr`](@ref)
 """
 snr2speff(::DVBS2x, snr) = snr < dvbs2x_thresholds_linear[1] ? 0.0 : dvbs2x_modcod_speff[floor(Int,_dvbs2x_itp_snr(snr))]
 
+# ╔═╡ 57500cea-75e5-473a-83c6-635f3ec3fa95
+snr2speff(::NR5G, snr) = snr < nr5g_thresholds_linear[1] ? 0.0 : _nr5g_itp_snr(snr)
+
+# ╔═╡ 774fc0f5-a11f-42eb-9bc0-3faa13ed24f4
+speff2snr(::NR5G, η) = η > nr5g_modcods_speff[end] ? NaN : _nr5g_itp_speff(η)
+
 # ╔═╡ 5203bbc5-4746-48cf-b209-ec618b223af4
 snr2speff(::Shannon, snr_lin) = log2(1 + snr_lin)
 
@@ -93,7 +189,7 @@ snr2speff(::Shannon, snr_lin) = log2(1 + snr_lin)
 speff2snr(::Shannon, speff) = 2^(speff) - 1
 
 # ╔═╡ 6c71d117-c2e5-4eb4-8abb-39109ffa30db
-export DVBS2x, Shannon, snr2speff, speff2snr
+export DVBS2x, NR5G, Shannon, snr2speff, speff2snr
 
 # ╔═╡ 602c9baf-96e5-4e2d-9e84-3bf177d137e9
 #=╠═╡ notebook_exclusive
@@ -130,6 +226,16 @@ b = sort(vec(a))
 @benchmark snr2speff.(DVBS2x(), $a)
   ╠═╡ notebook_exclusive =#
 
+# ╔═╡ 353923b3-ab53-4fdf-8eb3-f7a4d126bdf3
+#=╠═╡ notebook_exclusive
+@benchmark speff2snr.(NR5G(), $a)
+  ╠═╡ notebook_exclusive =#
+
+# ╔═╡ 5953946c-52fc-4297-bc3a-9aa2759c8be4
+#=╠═╡ notebook_exclusive
+@benchmark snr2speff.(NR5G(), $a)
+  ╠═╡ notebook_exclusive =#
+
 # ╔═╡ 080fdaa3-b9d7-4030-8bbc-88abcc5d37a3
 #=╠═╡ notebook_exclusive
 @benchmark snr2speff.(DVBS2x(), $b)
@@ -137,7 +243,7 @@ b = sort(vec(a))
 
 # ╔═╡ 2f49954a-8e37-4d84-accc-f5537e734331
 #=╠═╡ notebook_exclusive
-snr = range(-20,20,1000)
+snr = range(-6,20,1000)
   ╠═╡ notebook_exclusive =#
 
 # ╔═╡ 1a6110f9-4086-43ee-8a12-26755ac0ee8a
@@ -147,7 +253,29 @@ snr_lin = 10 .^ (snr./10)
 
 # ╔═╡ 87ca96a1-8f5a-4534-9e55-8f4bc4f0e437
 #=╠═╡ notebook_exclusive
-Plot(scatter(x = snr, y = snr2speff.(DVBS2x(), snr_lin)))
+Plot(
+	[
+		scatter(x = snr, y = snr2speff.(DVBS2x(), snr_lin), name = "DVB-S2X"),
+		scatter(x = snr, y = snr2speff.(NR5G(), snr_lin), name = "5G-NR"),
+	], Layout(
+		xaxis_title = "SNR (dB)", 
+		yaxis_title = "Spectral Efficiency",
+		legend = attr(
+			x = 0,
+			y = 1,
+			xanchor = "left",
+			bgcolor = "rgba(0,0,0,0)",
+		),
+		width = 500,
+		height = 400,
+		paper_bgcolor = "rgba(0,0,0,0)",
+	), config = PlotConfig(
+		toImageButtonOptions = Dict(
+			:filename => "dvb_5g_modcods",
+			:scale => 2,
+		)
+	)
+)
   ╠═╡ notebook_exclusive =#
 
 # ╔═╡ 6c13719f-3b65-4550-88e9-6f1c5cd84394
@@ -162,7 +290,12 @@ snr_lin_min = speff2snr.(DVBS2x(), speff)
 
 # ╔═╡ 0dcb4a08-b8ed-4a4b-8be2-7ccda056846a
 #=╠═╡ notebook_exclusive
-Plot(scatter(;x=speff, y=10log10.(snr_lin_min)))
+Plot(
+	[
+		scatter(;x=speff, y=10log10.(speff2snr.(DVBS2x(), speff)), name = "DVB-S2x"),
+		scatter(;x=speff, y=10log10.(speff2snr.(NR5G(), speff)), name = "5G-NR"),
+	], Layout(xaxis_title = "Target Spectral Efficiency", yaxis_title = "Minimum SNR (dB)");
+)
   ╠═╡ notebook_exclusive =#
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -184,7 +317,7 @@ PlutoUtils = "~0.5.3"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.0-rc2"
+julia_version = "1.7.1"
 manifest_format = "2.0"
 
 [[deps.AbstractPlutoDingetjes]]
@@ -586,20 +719,35 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 """
 
 # ╔═╡ Cell order:
+# ╠═1c0cf7ad-9579-4593-ae1f-441788c049ae
+# ╟─47a21d25-7710-4a93-8cbd-ad93cee68b74
 # ╠═d2056fd0-5419-11ec-2226-0b24c663de65
 # ╠═2c06b5c1-0345-444f-9664-338a60fedc83
 # ╠═6c71d117-c2e5-4eb4-8abb-39109ffa30db
+# ╟─78ad9ad5-7c06-41e1-940d-1aad1de7d5f2
 # ╠═35450d8d-cda5-46ba-884e-636abf023ac1
 # ╠═a1158d4b-38cc-4c9b-a949-e94e98c2b5f6
+# ╟─0345b4e1-49e6-4fc2-bc0e-6f179a0b1792
 # ╠═94032874-a85b-4632-bc24-c8c0154b518a
 # ╠═4d74427d-21e3-4736-ab29-ef65c7ea0490
+# ╟─a1c5f1c0-adc2-43f0-bd63-e4c6c7eca52b
 # ╠═5907b0b7-f81b-4cc3-a585-8f52fd191bae
 # ╠═37772abb-c600-4beb-a92e-7da3c3021370
 # ╠═27cda06f-de16-47f6-be79-324963b155c9
 # ╠═f03f7ebb-dad0-4766-8615-afde6478f59d
-# ╠═4b14629a-b25f-4f3c-94bd-14e77c044695
 # ╠═1f389615-64d5-4084-b4a7-e7838a19ff9b
+# ╟─eac20e32-59a3-44c3-9c68-31be56ad8ee9
+# ╠═ff6dd97b-66db-48e8-bd6d-bfccff28ba39
+# ╠═e48e3061-4004-4cee-8a93-7218e3ba5bbc
+# ╠═d2fbd3c2-215e-4859-861e-03a0b2efdc9b
+# ╠═b85c0bff-162d-402e-8d85-44d46b4af8a8
+# ╠═c2062a21-8027-4d49-85d6-6666f761cabb
+# ╠═29d90f34-0aeb-48d3-80b4-759bb0b77c2f
+# ╟─712f157b-8cc9-4034-8f54-8b57a7953683
+# ╠═4b14629a-b25f-4f3c-94bd-14e77c044695
 # ╠═215ca230-77fd-4bbb-a4f7-8af1a0c9bcb3
+# ╠═57500cea-75e5-473a-83c6-635f3ec3fa95
+# ╠═774fc0f5-a11f-42eb-9bc0-3faa13ed24f4
 # ╠═5203bbc5-4746-48cf-b209-ec618b223af4
 # ╠═154d61e7-9014-4cc0-b817-3257fcfe1826
 # ╠═602c9baf-96e5-4e2d-9e84-3bf177d137e9
@@ -609,6 +757,8 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╠═b959608b-2a49-4397-b974-9094480596ab
 # ╠═d0259918-920a-4b8e-b33d-a93da47425de
 # ╠═b6909885-61c0-4403-9639-c00ca7cd55e1
+# ╠═353923b3-ab53-4fdf-8eb3-f7a4d126bdf3
+# ╠═5953946c-52fc-4297-bc3a-9aa2759c8be4
 # ╠═080fdaa3-b9d7-4030-8bbc-88abcc5d37a3
 # ╠═2f49954a-8e37-4d84-accc-f5537e734331
 # ╠═1a6110f9-4086-43ee-8a12-26755ac0ee8a
